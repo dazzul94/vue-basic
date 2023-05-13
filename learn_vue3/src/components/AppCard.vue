6af39ed53f0c1d4ad3e6f974e3914daf6f951df0
dazzul94 <dazzul94@gmail.com>
2023-05-13 16:42:29 +0900

    SFC style 속성 사용

---
+++ learn_vue3/src/components/AppCard.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title red">Card title</h5>
+			<p class="card-text" :class="$style">
+				Some quick example text to build on the card title and make up the bulk
+				of the card's content.
+			</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref, useCssModule } from 'vue';
+
+export default {
+	setup() {
+		// const style = useCssModule();
+		// console.log('style:' + style);
+		const color = ref('red');
+		color.value = 'yellow';
+		return { color };
+	},
+};
+</script>
+
+<!-- <style scoped>
+.red {
+	color: red;
+}
+</style> -->
+<style>
+.red {
+	color: v-bind(color) !important;
+}
+</style>
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
