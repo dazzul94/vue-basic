71485c2440886f5aee54e47f089f4f298e9686e3
dazzul94 <dazzul94@gmail.com>
2023-05-21 18:05:04 +0900

    404 & 중첩 route

---
+++ vue3-posts/src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="text-center py-5">
+		<h2>Oops!</h2>
+		<h2>404 not found</h2>
+		<div class="text-muted">요청한 페이지를 찾을 수 없습니다.</div>
+		<div class="mt-4">
+			<RouterLink to="/">
+				<button class="btn btn-primary">HOME</button>
+			</RouterLink>
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
