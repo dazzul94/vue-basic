3454acc6ea0d0cea85a8819f7a15fb6c035d7d3f
dazzul94 <dazzul94@gmail.com>
2023-05-21 17:19:43 +0900

    router 설정

--- vue3-posts/src/views/AboutView.vue
+++ vue3-posts/src/views/AboutView.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h2>About View</h2>
 		<p>{{ $route.path }}</p>
-		<button class="btn btn-primary" @click="$router.push('/')">
+		<button class="btn btn-primary" @click="$router.push({ name: 'Home' })">
 			Home으로 이동
 		</button>
 	</div>
